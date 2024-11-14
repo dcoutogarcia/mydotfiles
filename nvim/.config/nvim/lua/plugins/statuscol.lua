1e473caf0ed2c6913c4456a7806783f118d89968
stoneboyriver <63312571+Stoneboyriver@users.noreply.github.com>
2024-11-14 22:20:51 +0100

    Initial commit

---
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,31 @@
+ return {
+   "luukvbaal/statuscol.nvim",
+   config = function()
+     local builtin = require("statuscol.builtin")
+     require("statuscol").setup({
+       -- configuration goes here, for example:
+        relculright = true,
+        -- segments = {
+        --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        --   {
+        --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+        --   {
+        --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        -- }
+       segments = {
+        { text = { "%C" }, click = "v:lua.ScFa" },
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        {
+          text = { builtin.lnumfunc, " " },
+          condition = { true, builtin.not_empty },
+          click = "v:lua.ScLa",
+        }
+      },
+     })
+   end,
+ }
