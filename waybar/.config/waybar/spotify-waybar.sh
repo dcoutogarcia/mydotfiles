11eecb41e816faff245e49ebff67948a55320f26
stoneboyriver <63312571+Stoneboyriver@users.noreply.github.com>
2025-02-18 18:38:17 +0100

    Añadido el script de spotify-waybar

---
+++ waybar/.config/waybar/spotify-waybar.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+while true
+do
+
+album=`playerctl -p spotify metadata album`
+artist=`playerctl -p spotify metadata artist`
+title=`playerctl -p spotify metadata title`
+status=`playerctl -p spotify status`
+
+if [ "$status" = "Playing" ]; then
+echo "{\"icon\": \"\",\"text\":\"   -  $title - $artist\", \"tooltip\": \"$album\"}"
+elif [ "$status" = "Paused" ]; then
+echo "{\"icon\": \"\",\"text\":\"   -  $title - $artist\", \"tooltip\": \"$album\"}"
+fi
+
+
+sleep 0.1
+
+done
